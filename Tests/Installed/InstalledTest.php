d82390a6cd67911bef3658230e0445bb9859a5b8
Ralph <ralph@huwiler.in>
2015-12-17 20:19:52 +0100

    Basic installed test

---
+++ Tests/Installed/InstalledTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Modules\Core\Traits\Testing\DatabaseMigrations;
+use Modules\Modules\Manager\ModuleManager;
+
+class InstalledTest extends TestCase
+{
+
+    use DatabaseMigrations, DatabaseTransactions, WithoutMiddleware;
+
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function tests_if_SocietyCMS_boots()
+    {
+        $this->visit('/')
+            ->see('SocietyCMS');
+    }
+
+    public function tests_if_backend_route_exists()
+    {
+        $this->visit(route('dashboard.index'))
+            ->see('Welcome to SocietyCMS');
+    }
+    
+}
