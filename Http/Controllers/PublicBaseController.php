0442aafef12dc6b4afba04cc473b5b78bb01acde
Ralph <ralph@huwiler.in>
2015-12-19 19:33:19 +0100

    cleanup public controller

--- Http/Controllers/PublicBaseController.php
+++ Http/Controllers/PublicBaseController.php
@@ -6,15 +6,7 @@ use Pingpong\Modules\Routing\Controller;
 
 abstract class PublicBaseController extends Controller
 {
-    /**
-     * @var \Modules\Core\Contracts\Authentication
-     */
-    protected $auth;
-
     public function __construct()
     {
-        $this->auth = app('Modules\Core\Contracts\Authentication');
-        // TODO Remove? no current user in public section
-        //view()->share('currentUser', $this->auth->check());
     }
 }
