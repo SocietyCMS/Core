75a91f09f2a39cd031abea79c655e844721c54a6
Ralph Huwiler <rhwilr@users.noreply.github.com>
2016-01-26 18:30:53 +0100

    remove unused import

--- Repositories/Eloquent/EloquentHashidsRepository.php
+++ Repositories/Eloquent/EloquentHashidsRepository.php
@@ -3,7 +3,6 @@
 namespace Modules\Core\Repositories\Eloquent;
 
 use Modules\Core\Repositories\HashidsRepository;
-use Vinkla\Hashids\Facades\Hashids;
 
 abstract class EloquentHashidsRepository extends EloquentBaseRepository implements HashidsRepository
 {
