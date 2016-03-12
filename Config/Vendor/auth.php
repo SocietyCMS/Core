046c7092c0766a5ae136378deb00fe220290fa3d
Ralph Huwiler <ralph@huwiler.rocks>
2016-03-12 12:49:53 +0100

    port new l5.2 settings, because entrust

--- Config/Vendor/auth.php
+++ Config/Vendor/auth.php
@@ -35,6 +35,34 @@ return [
     |
     */
     'table' => 'user__users',
+
+    /*
+   |--------------------------------------------------------------------------
+   | User Providers
+   |--------------------------------------------------------------------------
+   |
+   | All authentication drivers have a user provider. This defines how the
+   | users are actually retrieved out of your database or other storage
+   | mechanisms used by this application to persist your user's data.
+   |
+   | If you have multiple user tables or models you may configure multiple
+   | sources which represent each model / table. These sources may then
+   | be assigned to any extra authentication guards you have defined.
+   |
+   | Supported: "database", "eloquent"
+   |
+   */
+    'providers' => [
+        'users' => [
+            'driver' => 'eloquent',
+            'model' => Modules\User\Entities\Entrust\EloquentUser::class,
+        ],
+        // 'users' => [
+        //     'driver' => 'database',
+        //     'table' => 'users',
+        // ],
+    ],
+
     /*
     |--------------------------------------------------------------------------
     | Password Reset Settings
