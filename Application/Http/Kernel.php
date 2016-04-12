887f1aed627f27de8aee0020af96e0b49f071e73
Ralph Huwiler <ralph@huwiler.rocks>
2016-04-12 20:54:04 +0200

    moving Components into the Core

---
+++ Application/Http/Kernel.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Modules\Core\Application\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    /**
+     * The application's global HTTP middleware stack.
+     *
+     * @var array
+     */
+    protected $middleware = [
+        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \Modules\Core\Application\Http\Middleware\EncryptCookies::class,
+        \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+        \Illuminate\Session\Middleware\StartSession::class,
+        \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+    ];
+
+    /**
+     * The application's route middleware.
+     *
+     * @var array
+     */
+    protected $routeMiddleware = [
+    ];
+}
