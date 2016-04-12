26fee634b3bc9e70a472e5028a480502d3762c04
Ralph Huwiler <ralph@huwiler.rocks>
2016-04-12 23:56:33 +0200

    check for installed file

--- Console/Installers/Scripts/ProtectInstaller.php
+++ Console/Installers/Scripts/ProtectInstaller.php
@@ -22,13 +22,16 @@ class ProtectInstaller implements SetupScript
 
     /**
      * Fire the install script
-     * @param  Command   $command
+     * @param  Command $command
      * @return mixed
      * @throws Exception
      */
     public function fire(Command $command)
     {
-        if ($this->finder->isFile('.env') && ! $command->option('force')) {
+        if ($this->finder->isFile('.env') &&
+            $this->finder->isFile(storage_path('app/installed')) &&
+            !$command->option('force')
+        ) {
             throw new Exception('SocietyCMS has already been installed. You can already log into your administration.');
         }
     }
