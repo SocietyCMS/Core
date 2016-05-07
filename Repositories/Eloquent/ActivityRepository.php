a949ccd61359ea6e04ff8aefa6803a6d1a6cb089
Ralph Huwiler <ralph@huwiler.rocks>
2016-05-07 22:05:34 +0200

    cache activities

--- Repositories/Eloquent/ActivityRepository.php
+++ Repositories/Eloquent/ActivityRepository.php
@@ -22,10 +22,33 @@ class ActivityRepository extends EloquentBaseRepository
      */
     public function latestGroupedByDate($amount = 10)
     {
+
+        if(!config('repository.cache.enabled', true) || $this->isSkippedCache()) {
+            return $this->latestGroupedByDateSkippingCache($amount);
+        }
+
+        $key = $this->getCacheKey('latestGroupedByDate', func_get_args());
+
+        $minutes = $this->getCacheMinutes();
+        $value = $this->getCacheRepository()->remember($key, $minutes, function () use ($amount) {
+            return $this->latestGroupedByDateSkippingCache($amount);
+        });
+
+        return $value;
+    }
+
+    /**
+     * Take the latest records and group by date.
+     *
+     * @param int $amount
+     * @return mixed
+     */
+    protected function latestGroupedByDateSkippingCache($amount = 10)
+    {
         $this->applyCriteria();
         $this->applyScope();
         $model = $this->model->select('*', \DB::raw('CONCAT(DATE_ADD(DATE_FORMAT(created_at, "%Y-%m-%d %H:00:00"),INTERVAL IF(MINUTE(created_at) < 30, 0, 1) HOUR), "-", subject_id) AS grouped_created_at'), \DB::raw('count(*) as count_in_group'))
-            ->latest()
+            ->orderby('created_at', 'desc')
             ->groupBy('grouped_created_at')
             ->with('subject')
             ->take($amount)
