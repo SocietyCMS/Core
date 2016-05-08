eb18cf2b171985123de205ecd5351b48c60328f8
Ralph Huwiler <ralph@huwiler.rocks>
2016-05-08 13:13:21 +0200

    404 error message

--- Resources/lang/de/core.php
+++ Resources/lang/de/core.php
@@ -9,4 +9,11 @@ return [
     ],
 
     'permission denied' => 'Zugriff verweigert. (erfordert die Berechtigung: ":permission")',
+
+    'error404' => [
+        'error 404' => 'Fehler 404',
+        'page not found' => 'Seite nicht gefunden',
+        'message' => 'Die Seite die du suchst wurde leider nicht gefunden.',
+        'back to dashboard' => 'Zurück zum Dashboard'
+    ]
 ];
--- Resources/lang/en/core.php
+++ Resources/lang/en/core.php
@@ -9,4 +9,11 @@ return [
     ],
 
     'permission denied' => 'Permission denied. (required permission: ":permission")',
+
+    'error404' => [
+        'error 404' => 'Error 404',
+        'page not found' => 'Page not found',
+        'message' => 'The page you are looking for could not be found, Sorry!',
+        'back to dashboard' => 'Back to Dashboard'
+    ]
 ];
